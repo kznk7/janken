c4640966026eaaf016fa784402dc487900d2e180
kznk7 <e1b18069@st.oit.ac.jp>
2020-10-19 01:06:11 +0900

    ログインしている全ユーザの名前を表示

--- src/main/java/oit/is/z1438/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z1438/kaizi/janken/controller/Lec02Controller.java
@@ -1,5 +1,8 @@
 package oit.is.z1438.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z1438.kaizi.janken.model.Janken;
+import oit.is.z1438.kaizi.janken.model.Entry;
 
 @Controller
 @RequestMapping("/lec02")
 public class Lec02Controller {
 
+  @Autowired
+  private Entry entry;
+
   @GetMapping
-  public String lec02() {
+  public String lec02(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("login_user", loginUser);
+    model.addAttribute("entry", this.entry);
     return "lec02.html";
   }
 
