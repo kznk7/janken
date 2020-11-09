350a150a3e2e51aa76431f5a88a7c56978fc92c3
kznk7 <e1b18069@st.oit.ac.jp>
2020-11-09 19:58:54 +0900

    手を選んだ後に対戦相手が手を選ぶまで待つ画面に遷移する

--- src/main/java/oit/is/z1438/kaizi/janken/model/MatchMapper.java
+++ src/main/java/oit/is/z1438/kaizi/janken/model/MatchMapper.java
@@ -10,8 +10,11 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface MatchMapper {
 
-  @Select("SELECT * from matches")
+  @Select("SELECT * FROM matches")
   ArrayList<Match> selectAllMatches();
+
+  @Select("SELECT * FROM matches WHERE is_active = true")
+  ArrayList<Match> selectActiveMatche();
 
   @Insert("INSERT INTO matches (user_1,user_2,user_1_hand,user_2_hand,is_active) VALUES (#{user_1},#{user_2},#{user_1_hand},#{user_2_hand},#{is_active});")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
